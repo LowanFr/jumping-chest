c2a0de339aba95bc555e90023bd45e51d32bf30d
Lowan <lowan@unosial.com>
2022-09-22 15:25:34 +0200

    📝 Documentations des fonctions créées

--- fonctions_SDL.c
+++ fonctions_SDL.c
@@ -1,36 +1,32 @@
 #include "fonctions_SDL.h"
 
-SDL_Texture* charger_image(const char* nomfichier, SDL_Renderer* renderer){
-    //Charger une image
-    SDL_Surface* surface = SDL_LoadBMP(nomfichier);
-    //Convertir la surface de l'image au format texture
-    SDL_Texture* image = SDL_CreateTextureFromSurface(renderer,surface);
-    return image;
+SDL_Texture* charger_image(const char* nomFichier, SDL_Renderer* renderer) {
+    // Chargement de l'image à partir du chemin
+    SDL_Surface* surface = SDL_LoadBMP(nomFichier);
+
+    // Conversion de la surface en texture
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    return texture;
 }
 
+SDL_Texture* charger_image_transparente(const char* nomFichier, SDL_Renderer* renderer, Uint8 r, Uint8 g, Uint8 b) {
+    // Chargement de l'image à partir du chemin
+    SDL_Surface* surface = SDL_LoadBMP(nomFichier);
 
-SDL_Texture* charger_image_transparente(const char* nomfichier, SDL_Renderer* renderer, Uint8 r, Uint8 g, Uint8 b){
-    //Charger une image
-    SDL_Surface* surface = SDL_LoadBMP(nomfichier);
-    
-    // Récupérer la valeur (RGB) du pixel au format donné.
-    Uint32 key = SDL_MapRGB(surface->format, r, g, b);
-    // Définir la couleur (pixel transparent) dans une surface.
-    int reussite = SDL_SetColorKey(surface, SDL_TRUE,key) ;
-    
-    //Convertir la surface de l'image au format texture
-    SDL_Texture* image = SDL_CreateTextureFromSurface(renderer,surface);
+    // Récupération de la valeur (RGB) au format donné puis application sur la surface
+    Uint32 colorKey = SDL_MapRGB(surface->format, r, g, b);
+    SDL_SetColorKey(surface, SDL_TRUE, colorKey);
 
-
-    return image;
+    // Conversion de la surface en texture
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    return texture;
 }
 
-SDL_Texture* charger_texte(const char* message, SDL_Renderer* renderer,
-TTF_Font *font, SDL_Color color){
+SDL_Texture* charger_texte(const char* message, SDL_Renderer* renderer, TTF_Font *font, SDL_Color color) {
     // Écrire le texte sur une surface SDL
     SDL_Surface* surface = TTF_RenderText_Solid(font, message, color);
+
     //Convertir la surface de l'image au format texture
     SDL_Texture* image = SDL_CreateTextureFromSurface(renderer,surface);
-
     return image;
 }
--- fonctions_SDL.h
+++ fonctions_SDL.h
@@ -1,32 +1,34 @@
-#ifndef SDL2
-#define SDL2
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
+#ifndef FONCTIONS_SDL_H
+#define FONCTIONS_SDL_H
+#include <SDL_ttf.h>
+#include <SDL.h>
 
 /**
- * \brief La fonction charge une image
- * \param nomfichier nom du fichier à charger
- * \param renderer 
-*/
-SDL_Texture* charger_image(const char* nomfichier, SDL_Renderer* renderer);
+ * Chargement d'une image et renvoie un pointeur vers la texture.
+ * @param nomFichier Nom/Chemin vers un fichier
+ * @param renderer Rendu
+ * @return Texture avec l'image
+ */
+SDL_Texture* charger_image(const char* nomFichier, SDL_Renderer* renderer);
 
 /**
- * \brief La fonction charge une image transparente
- * \param nomfichier nom du fichier à charger
- * \param renderer 
- * \param r red
- * \param g green
- * \param b blue
-*/
-SDL_Texture* charger_image_transparente(const char* nomfichier, SDL_Renderer* renderer, Uint8 r, Uint8 g, Uint8 b) ;
+ * Chargement d'une image et renvoie un pointeur vers la texture.
+ * @param nomFichier Nom/Chemin vers un fichier
+ * @param renderer Rendu
+ * @param r Taux de rouge
+ * @param g Taux de vert
+ * @param b Taux de bleu
+ * @return Texture avec l'image (avec transparence)
+ */
+SDL_Texture* charger_image_transparente(const char* nomFichier, SDL_Renderer* renderer, Uint8 r, Uint8 g, Uint8 b);
 
 /**
- * \brief La fonction charge un texte
- * \param message message à afficher
- * \param renderer 
- * \param font
- * \param color
-*/
-SDL_Texture* charger_texte(const char* message, SDL_Renderer* renderer,
-TTF_Font *font, SDL_Color color);
+ * Chargement d'un texte et renvoie un pointeur vers la texture.
+ * @param message Message à afficher
+ * @param renderer Rendu
+ * @param font Police d'écriture
+ * @param color Couleur
+ * @return Texture avec le texte
+ */
+SDL_Texture* charger_texte(const char* message, SDL_Renderer* renderer, TTF_Font *font, SDL_Color color);
 #endif
