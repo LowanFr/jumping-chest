b904f673638916d96b1f3f1581d5d212709cc52b
ViiRtuOzZz <paul54114@hotmail.fr>
2022-09-24 18:32:48 +0200

    V1.1.0

---
+++ archive/addition.c
@@ -0,0 +1,5 @@
+#include "addition.h"
+
+int somme(int a, int b){
+    return a+b;
+}
---
+++ constante.h
@@ -0,0 +1,24 @@
+#ifndef CONSTANTE_H
+#define CONSTANTE_H
+
+/**
+ * \brief Vitesse du joueur sur l'axe X (marche)
+ */
+#define VITESSE_X_MARCHE 10
+
+/**
+ * \brief Vitesse du joueur sur l'axe Y (saut)
+ */
+#define VITESSE_Y_SAUT 30
+
+/**
+ * \brief Gravité, sert au joueur de retomber
+ */
+#define GRAVITE 2
+
+
+
+
+
+
+#endif
