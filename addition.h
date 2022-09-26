48591dee1ca2d6c65275957c31a3d59c9935839b
Lowan <lowan@unosial.com>
2022-09-26 22:23:34 +0200

    🚚 Update addition path

--- addition.h
+++
@@ -1,6 +0,0 @@
-#ifndef ADDITION_H
-#define ADDITION_H
-
-int somme(int a,int b);
-
-#endif
---
+++ archive/addition.h
@@ -0,0 +1,12 @@
+#ifndef ADDITION_H
+#define ADDITION_H
+
+/**
+ * Addition de deux nombres
+ * @param a Premier nombre
+ * @param b Second nombre
+ * @return Résultat
+ */
+int somme(int a,int b);
+
+#endif
