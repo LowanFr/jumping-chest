a89ded57daf6da28d159385ec8bf7b3e7c29f0a7
Paul Schneider <schnei441u@aig-ats-312-l12.fst.site.univ-lorraine.fr>
2022-10-26 12:02:02 +0200

    Ajout gestion texture

--- constante.h
+++ constante.h
@@ -1,6 +1,30 @@
 #ifndef CONSTANTE_H
 #define CONSTANTE_H
 
+//4,72 coord 1er bloc
+//16*4
+
+/**
+ * \brief Nombre de textures dans une tilemap
+ */
+#define NBR_DE_TEXTURES 33
+/**
+ * \brief coordonnée de x de la première 
+ */
+#define X_PREMIERE_TEXTURE 4
+/**
+ * \brief coordonnée de y de la première 
+ */
+#define Y_PREMIERE_TEXTURE 72
+/**
+ * \brief Taille d'une seule texture
+ */
+#define TAILLE_TEXTURES 64
+/**
+ * \brief Taille de la ligne bleu qui sépare deux textures
+ */
+#define DECALAGE_TEXTURE 4
+
 /**
  * \brief Vitesse du joueur sur l'axe X (marche)
  */
