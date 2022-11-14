47c9c05634fc22b46b681aaaaae580c619d9b062
Lowan <lowan@unosial.com>
2022-11-14 10:59:37 +0100

    🚩 Updating main

--- main.c
+++ main.c
@@ -1,85 +1,78 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include "fonctions_SDL.h"
-#include "world.h"
-#include "joueur.h"
-#include "constante.h"
-#include "graphique.h"
-#include "clavier.h"
+#include "player.h"
+#include "graphic.h"
 
-//Couleur bleu texture rgb(55,78,211)
-
+// La taille de l'écran
 #define SCREEN_H 720
 #define SCREEN_W 1280
 
-void init(SDL_Window **window, SDL_Renderer **renderer, ressources_t *ressources, world_t *world, etat_clavier_t *touches, joueur_t *joueur){
-    init_sdl(window,renderer,SCREEN_W, SCREEN_H);
+/**
+ * @brief Initialisation du jeu
+ * @param window La fenêtre
+ * @param renderer Le rendu
+ * @param ressources Les ressources
+ * @param world Le monde
+ * @param keys Les touches du clavier
+ * @param player Le joueur
+ */
+void init(SDL_Window **window, SDL_Renderer **renderer, ressources_t *ressources, world_t *world, keyboard_status_t *keys, player_t *player) {
+    init_sdl(window,renderer, SCREEN_W, SCREEN_H);
     init_world(world);
-    init_touches(touches);
+    init_touches(keys);
     init_ressources(*renderer, ressources);
-    init_joueur(joueur);
+    init_player(player);
 }
 
 /**
- * La fonction principale. C'est la première fonction qui est appelée lors de l'exécution du programme.
- * @param argc Le nombre d'arguments passés au programme.
- * @param argv C'est un tableau de chaînes de caractères qui contient les arguments de la ligne de commande.
+ * @brief La fonction principale. C'est la première fonction qui est appelée lors de l'exécution du programme.
  */
 int main() {
-    SDL_Window* window; // Déclaration de la fenêtre
+    SDL_Window* window;
     SDL_Renderer* renderer;
     ressources_t ressources;
     world_t world;
-    etat_clavier_t touches;
-    joueur_t joueur;
+    keyboard_status_t keys;
+    player_t player;
 
-    init(&window, &renderer, &ressources, &world, &touches, &joueur);
+    // Initialisation du jeu
+    init(&window, &renderer, &ressources, &world, &keys, &player);
 
     //Changement de la couleur de fond
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
     //Chargement de la map
-    int temps_fin = 0;
+    int tempsFin = 0;
 
     // Boucle principale
     while (!world.end) {
         // Suppression de l'ancien rendu
         clear_renderer(renderer);
 
-        // Mise à jour du rendu
-        
+        // Mise à jour des textures
         SDL_RenderCopy(renderer, ressources.background, NULL, NULL);
-        SDL_RenderCopy(renderer, ressources.player, &world.joueur->SrcR, &world.joueur->DestR);
+        SDL_RenderCopy(renderer, ressources.player, &world.player->SrcR, &world.player->DestR);
 
-
-        for(int i = 0; i<world.map->nb_ligne; ++i){
+        for(int i = 0; i<world.map->nb_row; ++i){
             for(int j = 0; j<world.map->nb_col; ++j){
                 SDL_RenderCopy(renderer, ressources.blocks, &world.blocks[world.map->tab[i][j]].SrcR, &world.map->DestR[i][j]);
             }
         }
-        
-        
 
         // Récupération des événements
-        SDL_PollEvent(&touches.events);
+        SDL_PollEvent(&keys.events);
+        refresh_keys(&world, &keys);
 
-        refresh_keys(&world, &touches);
+        // Déplacement du player
+        player_movement(&keys, &player, world.player);
 
-        
-
-        // Déplacement du joueur
-        deplacement_joueur(&touches, &joueur, world.joueur);
-
-        
         // Mise à jour de l'écran avec le rendu
         SDL_RenderPresent(renderer);
-        
 
         // Ralentissement pour un affichage fluide
-        if (SDL_GetTicks() < (temps_fin + 1000 / FPS)){
-            SDL_Delay((temps_fin + 1000 / FPS) - SDL_GetTicks());
+        if (SDL_GetTicks() < (tempsFin + 1000 / FPS)){
+            SDL_Delay((tempsFin + 1000 / FPS) - SDL_GetTicks());
         }
-        temps_fin = SDL_GetTicks();
+        tempsFin = (int) SDL_GetTicks();
     }
     
     // Quitter SDL
@@ -91,17 +84,16 @@ int main() {
     SDL_DestroyTexture(ressources.blocks);
     SDL_Quit();
     
-    desallouer_tab_2D(world.map->tab, world.map->nb_ligne);
+    desallouer_tab_2D(world.map->tab, world.map->nb_row);
     
-    for (int i = 0; i < world.map->nb_ligne; i++) { // Libère toutes les lignes
+    for (int i = 0; i < world.map->nb_row; i++) { // Libère toutes les lignes
         free(world.map->DestR[i]);
     }
     free(world.map->DestR);
     
     free(world.map);
     free(world.blocks);
-    free(world.joueur);
-    
+    free(world.player);
 
     return EXIT_SUCCESS;
 }
