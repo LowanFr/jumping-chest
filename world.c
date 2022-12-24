21cec38ad682e01479b8e14a6293d6f2a64d095a
Lowan <lowan@unosial.com>
2022-12-24 12:21:16 +0100

    🩹 Fix game level

--- world.c
+++ world.c
@@ -180,9 +180,10 @@ void refresh_level(SDL_Renderer *renderer, game_t *game, ressources_t *ressource
         SDL_Delay(2000);
 
         // Modifie les textures en fonction du niveau
-        if (strcmp(game->level, "classic") == 0) game->level = "snow";
-        else if (strcmp(game->level, "snow") == 0) game->level = "lava";
-        else game->level = "END";
+        if (strcmp(game->level, "classic") == 0) sprintf(game->level, "snow");
+        else if (strcmp(game->level, "snow") == 0) sprintf(game->level, "lava");
+        else
+            sprintf(game->level, "END");
 
         // Vérifie si s'il y a un prochain niveau
         if (strcmp(game->level, "END") != 0) {
@@ -191,7 +192,7 @@ void refresh_level(SDL_Renderer *renderer, game_t *game, ressources_t *ressource
             init_world(game, world, false);
         } else {
             new_level(renderer, game, ressources);
-            game->level = "classic";
+            sprintf(game->level, "classic");
             world->end = true;
             world->go_menu = true;
             world->cycles_pause = 0;
